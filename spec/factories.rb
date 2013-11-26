627db2076252ada2a7648bbac52b0af91694c7b5
Julien Puchol <julien.puchol@insa-lyon.Fr>
2013-11-26 14:55:09 +0100

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    name     "Julien Puchol"
-    email    "julien@puchol.fr"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
     password "foobar"
     password_confirmation "foobar"
+
+    factory :admin do
+    	admin true
+    end
   end
 end
